a86d5581d998845901477dba0a5825fe132ac1e4
Jaysup29 <52956176+Jaysup29@users.noreply.github.com>
2021-09-25 13:25:53 +0800

    sample

---
+++ nav.php
@@ -0,0 +1,40 @@
+<nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+    <li class="nav-item mb-2">
+      <a href="3" class="nav-link">
+        <i class="nav-icon fas fa-columns"></i>
+          <p>Dashboard</p>
+      </a>
+    </li>
+    <li class="nav-item mb-2">
+      <a href="#" class="nav-link">
+        <i class="nav-icon fas fa-inbox"></i>
+        <p>Receiving</p>
+      </a>
+    </li>
+    <li class="nav-item mb-2">
+      <a href="#" class="nav-link">
+        <i class="nav-icon fas fa-dice-d6"></i>
+        <p>
+          Orders
+        </p>
+      </a>
+    </li>
+    <li class="nav-item mb-2">
+      <a href="#" class="nav-link">
+        <i class="nav-icon fas fa-file-alt"></i>
+        <p>
+          Reports
+        </p>
+      </a>
+    </li>
+    <li class="nav-item mb-2">
+      <a href="#" class="nav-link">
+        <i class="nav-icon fas fa-cog"></i>
+        <p>
+          Settings
+        </p>
+      </a>
+    </li>
+    </ul>
+</nav>
