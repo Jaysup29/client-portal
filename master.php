9339a6ce444ff88933e77cb9435e91b3f970e03f
Jaysup29 <52956176+Jaysup29@users.noreply.github.com>
2021-09-25 12:01:54 +0800

    sample

---
+++ master.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./node_modules/@fortawesome/fontawesome-free/css/all.min.css">
+    <title>Client Portal</title>
+</head>
+<body>
+<div class="wrapper">
+<!--
+    //preloader
+    <div class="preloader flex-column justify-content-center align-items-center">
+        <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+    </div>
+-->
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+            </li>
+        </ul>
+    </nav>
+</div>
+    
+    <script href="./js/scripts.js"></script>
+    <script href="./node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script href="./node_modules/chart.js/dist/chart.js"></script>
+</body>
+</html>
